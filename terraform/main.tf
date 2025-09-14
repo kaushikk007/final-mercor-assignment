9ca0b0f89c11ac7809636f4f31a9909907b4fcdd
Anushka Panwar <anu@Anushkas-MacBook-Pro.local>
2025-09-15 03:07:37 +0530

    green deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,8 +132,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "bluegreen-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "512"
+  memory                   = "1024"
 
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([{
