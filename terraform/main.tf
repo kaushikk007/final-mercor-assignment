95340a69265386a1ef24f3577a1fe8f6b2d6424b
Anushka Panwar <anu@Anushkas-MacBook-Pro.local>
2025-09-15 02:32:38 +0530

    added role arn

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,6 +103,30 @@ resource "aws_lb_listener" "this" {
   }
 }
 
+# ECS Execution Role (needed for pulling from ECR and writing logs)
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole-${random_string.suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach AWS managed policy for ECS task execution
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 # ---- ECS Task + Service ----
 resource "aws_ecs_task_definition" "app" {
   family                   = "bluegreen-task"
@@ -111,6 +135,7 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
 
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([{
     name      = "app"
     image     = var.image_url
