fe3777823be8058caa3fcd7404dfa8021951e314
Anushka Panwar <anu@Anushkas-MacBook-Pro.local>
2025-09-15 03:36:27 +0530

    added igw

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,33 @@ resource "aws_subnet" "public_b" {
   availability_zone = "us-east-1b"
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+# Associate Route Table with Subnets
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 # Security group for ALB
 resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.this.id
